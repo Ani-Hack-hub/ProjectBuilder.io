519423d7a7d06ca0258a5d68ba2b5fdb59839e35
Ani-Hack-hub <aniruddhanarasimman@gmail.com>
2024-05-08 22:19:51 +0530

    works db done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import connection from './Db.js'
 import UserRouter from './Routes/User.js'
 import Configrouter from './Routes/Db_config.js'
+import worksRouter from './Routes/Works.js'
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/user', UserRouter)
 app.use('/config', Configrouter)
+app.use('/works', worksRouter)
 
 app.get('/', (req, res)=>{
     connection.query('desc userdetails', (err, result)=>{
